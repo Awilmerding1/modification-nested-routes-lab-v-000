2f4406714dc1ac69243b4a06026a1b74b0e067a1
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-02 14:32:38 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,12 +4,12 @@ module ArtistsHelper
   end
   
   def artist_select(song, artist)
-    if params[:artist_id]
-    # if song.artist.nil?
-    #   select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-    # else 
-    #   hidden_field_tag "song[artist_id]", song.artist_id
-    # end
+    
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else 
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
   end
 
   
